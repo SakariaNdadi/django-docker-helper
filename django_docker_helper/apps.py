99dce55ff1b6160e0b8078c9ec77118ee5278aeb
SakariaNdadi <oipapi.ndadi@gmail.com>
2024-03-05 14:41:48 +0200

    name edits

--- django_docker_helper/apps.py
+++ django_docker_helper/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class DockerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'docker'
+    name = 'django_docker_helper'
+    label = "docker"
